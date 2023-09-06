c001bdbc878961f75a2e7ab7096142ce0d463d3e
Kamil Marut <kamil.marut@finmatics.com>
2023-09-06 19:45:30 +0800

    feat: Create site

---
+++ src/components/ContactForm.svelte
@@ -0,0 +1,56 @@
+<div class="py-12 px-8 md:px-10 bg-white rounded-4xl max-w-xl mx-auto lg:mr-0">
+    <h3 class="text-3xl tracking-tight mb-8">Contact Us</h3>
+    <form>
+        <input
+            class="block w-full py-4 px-8 h-16 mb-6 text-coolGray-500 bg-white border border-coolGray-400 rounded-full"
+            type="text"
+            placeholder="Name"
+        />
+        <input
+            class="block w-full py-4 px-8 h-16 mb-6 text-coolGray-500 bg-white border border-coolGray-400 rounded-full"
+            type="email"
+            placeholder="E-mail"
+        />
+        <div class="relative">
+            <span
+                class="absolute top-1/2 right-0 mr-10 transform -translate-y-1/2"
+            >
+                <svg
+                    width="12"
+                    height="8"
+                    viewBox="0 0 12 8"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M1.33203 1.66439L5.9987 6.33105L10.6654 1.66439"
+                        stroke="black"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                    />
+                </svg>
+            </span>
+            <select
+                class="relative block w-full py-4 px-8 h-16 mb-6 text-coolGray-500 bg-transparent border border-coolGray-400 rounded-full appearance-none outline-none"
+            >
+                <option selected="">What can we help you with?</option>
+                <option>IT consulting</option>
+                <option>Software architecture</option>
+                <option>Cloud engineering</option>
+                <option>Web development</option>
+                <option>Custom software development</option>
+                <option>Other</option>
+            </select>
+        </div>
+        <textarea
+            class="w-full h-52 py-5 px-8 text-coolGray-500 bg-white border border-coolGray-400 rounded-3xl resize-none mb-8"
+            placeholder="Message"
+            rows="3"
+        />
+        <button
+            class="block w-full py-5 px-8 text-center font-medium text-white leading-none bg-black hover:bg-blue-500 rounded-full transition duration-150"
+            type="submit">Send message</button
+        >
+    </form>
+</div>
