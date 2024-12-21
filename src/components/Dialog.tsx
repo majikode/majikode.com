875d9a2c1cc24cce12e9a9a2da200f05e2ce33b8
Kamil Marut <kamil.marut@finmatics.com>
2024-12-21 20:15:18 +0100

    feat: Close the dialog on click outside

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -5,7 +5,12 @@ export default function Dialog({
     ref,
 }: { children: React.ReactNode; ref: React.RefObject<HTMLDialogElement | null> }) {
     return (
-        <dialog ref={ref} className="max-w-4xl gap-4 p-6 rounded-md shadow-md bg-neutral-900 text-white">
+        // biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>We don't want to close the dialog on key press.</explanation>
+        <dialog
+            ref={ref}
+            className="max-w-4xl gap-4 p-6 rounded-md shadow-md bg-neutral-900 text-white"
+            onClick={(e) => e.target === e.currentTarget && ref.current?.close()}
+        >
             <div className="flex flex-col items-center gap-4">
                 {children}
                 <form method="dialog" className="self-end">
