b6da1af286bb4760b69b2cff42dfba1c23fa6d5b
Kamil Marut <kamil@kamilmarut.com>
2025-05-14 17:58:37 +0200

    feat: Replace Plausible analytics with GoatCounter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import PlausibleProvider from "next-plausible";
 import { Work_Sans } from "next/font/google";
 import "@/styles/globals.css";
+import Script from "next/script";
 
 const work_sans = Work_Sans({ weight: "400", subsets: ["latin"], display: "swap" });
 
@@ -18,15 +19,14 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <Script
+                data-goatcounter="https://majikode.goatcounter.com/count"
+                async
+                src="//gc.zgo.at/count.js"
+                strategy="beforeInteractive"
+            />
             <body className={work_sans.className}>
-                <PlausibleProvider
-                    domain="majikode.com"
-                    customDomain="https://plausible.royal-puffin.net"
-                    trackOutboundLinks={true}
-                    selfHosted={true}
-                >
-                    <div className="container mx-auto max-w-xl my-8">{children}</div>
-                </PlausibleProvider>
+                <div className="container mx-auto max-w-xl my-8">{children}</div>
             </body>
         </html>
     );
